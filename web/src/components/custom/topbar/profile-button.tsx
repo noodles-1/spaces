fc97064f2a55075757a5ca5e27a8f195b035057d
chowlong <adrianeroa081202@gmail.com>
2025-04-20 16:43:54 +0800

    feat(web): profile dialog

--- web/src/components/custom/topbar/profile-button.tsx
+++ web/src/components/custom/topbar/profile-button.tsx
@@ -1,29 +1,61 @@
-"use client";
+import { PenLine, UserRound } from "lucide-react";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
-import { UserRound } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export function ProfileButton() {
-    const pathname = usePathname();
-
     return (
-        <div className="flex h-full items-center">
-            <Button variant="ghost" className="mx-4 h-[4rem] p-0">
-                <Link
-                    href="/profile"
-                    className={`flex h-full w-full items-center gap-3 px-4 ${pathname === "/profile" ? "text-[#bfc7ff]" : "text-zinc-300"} `}
-                >
-                    <div
-                        className={`flex h-8 w-8 items-center justify-center rounded-full outline-2 ${pathname === "/profile" ? "outline-[#7076a7]" : "outline-zinc-500"} `}
-                    >
-                        <UserRound />
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="flex items-center h-full">
+                    <Button variant="ghost" className="mx-4 h-[4rem] p-0 group cursor-pointer">
+                        <div className="flex items-center w-full h-full gap-3 px-4">
+                            <div className="flex h-8 w-8 items-center justify-center rounded-full outline-2 outline-zinc-500 group-hover:outline-[#7076a7]">
+                                <UserRound />
+                            </div>
+                            <span className="group-hover:text-[#bfc7ff]"> Username </span>
+                        </div>
+                    </Button>
+                </div>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="font-medium"> Profile </DialogTitle>
+                    <DialogDescription> Edit your profile details </DialogDescription>
+                </DialogHeader>
+                <section className="flex gap-8 py-4">
+                    <div className="relative flex h-[120px] w-[120px] items-center justify-center rounded-full outline-2 group">
+                        <UserRound height={50} width={50} />
+                        <div className="absolute flex items-center justify-center w-full h-full transition-opacity duration-150 rounded-full opacity-0 cursor-pointer group-hover:opacity-60 bg-zinc-600">
+                            <PenLine color="white" height={40} width={40} opacity={70} />
+                        </div>
                     </div>
-                    <span> Username </span>
-                </Link>
-            </Button>
-        </div>
+                    <section className="space-y-4 text-sm">
+                        <div className="flex flex-col gap-2">
+                            <span className="font-medium text-zinc-300"> Username: </span>
+                            <Input />
+                        </div>
+                        <div className="flex gap-1">
+                            <span className="font-medium text-zinc-300"> Email: </span>
+                            <span> example@email.com </span>
+                        </div>
+                    </section>
+                </section>
+                <DialogFooter>
+                    <Button variant="outline" className="cursor-pointer hover:text-red-300"> Logout </Button>
+                    <Button className="cursor-pointer"> Save changes </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 }
