dfc2cb73d809b192a3e551f39ea92f0f00ad0614
chowlong <adrianeroa081202@gmail.com>
2025-07-22 21:12:20 +0800

    web(sidebar): shared folders

--- web/src/components/custom/sidebar/sidebar.tsx
+++ web/src/components/custom/sidebar/sidebar.tsx
@@ -3,33 +3,42 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { Home, Star, Database, Trash } from "lucide-react";
+import { Home, Star, Database, Trash, FolderSymlink } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { StorageCapacity } from "@/components/custom/sidebar/storage-capacity";
 import { NewFolder } from "@/components/custom/sidebar/new-folder";
 
-const items = [
-    {
-        title: "Home",
-        url: "/spaces/home",
-        icon: Home,
-    },
-    {
-        title: "Starred",
-        url: "/spaces/starred",
-        icon: Star,
-    },
-    {
-        title: "Trash",
-        url: "/spaces/trash",
-        icon: Trash,
-    },
-    {
-        title: "Storage",
-        url: "/spaces/storage",
-        icon: Database,
-    },
+const itemGroups = [
+    [
+        {
+            title: "Home",
+            url: "/spaces/home",
+            icon: Home,
+        },
+        {
+            title: "Starred",
+            url: "/spaces/starred",
+            icon: Star,
+        },
+        {
+            title: "Shared folders",
+            url: "/spaces/shared",
+            icon: FolderSymlink,
+        },
+    ],
+    [
+        {
+            title: "Trash",
+            url: "/spaces/trash",
+            icon: Trash,
+        },
+        {
+            title: "Storage",
+            url: "/spaces/storage",
+            icon: Database,
+        },
+    ]
 ];
 
 export function Sidebar() {
@@ -43,31 +52,34 @@ export function Sidebar() {
                 </p>
             </Link>
             <section className="flex justify-center">
-                <div className="mt-5 flex w-[90%] flex-col gap-4">
+                <div className="mt-6 flex w-[90%] flex-col gap-7">
                     <NewFolder />
-                    {items.map((item: (typeof items)[0], idx: number) => (
-                        <Button
-                            variant="outline"
-                            key={idx}
-                            className={`w-full rounded-xl p-0 ${idx % 2 === 0 && "mt-5"} ${item.title === "Storage" && "h-fit"} `}
-                        >
-                            <Link
-                                href={item.url}
-                                className={`flex h-full w-full px-6 ${pathname === item.url && "text-[#bfc7ff]"} ${item.title === "Storage" && "my-3 flex-col items-start justify-center gap-2"} `}
-                            >
-                                <div className="flex items-center gap-4">
-                                    <item.icon />
-                                    <span className="font-normal">
-                                        {" "}
-                                        {item.title}{" "}
-                                    </span>
-                                </div>
-                                {item.title === "Storage" && (
-                                    <StorageCapacity />
-                                )}
-                            </Link>
-                        </Button>
-                    ))}
+                    {itemGroups.map((itemGroup, i) =>
+                        <section key={i} className="flex flex-col gap-4">
+                            {itemGroup.map((item, j) =>
+                                <Button
+                                    variant="outline"
+                                    key={j}
+                                    className={`w-full rounded-xl p-0 ${item.title === "Storage" && "h-fit"} `}
+                                >
+                                    <Link
+                                        href={item.url}
+                                        className={`flex h-full w-full px-6 ${pathname === item.url && "text-[#bfc7ff]"} ${item.title === "Storage" && "my-3 flex-col items-start justify-center gap-2"} `}
+                                    >
+                                        <div className="flex items-center gap-4">
+                                            <item.icon />
+                                            <span className="font-normal">
+                                                {item.title}
+                                            </span>
+                                        </div>
+                                        {item.title === "Storage" && (
+                                            <StorageCapacity />
+                                        )}
+                                    </Link>
+                                </Button>
+                            )}
+                        </section>
+                    )}
                 </div>
             </section>
         </div>
